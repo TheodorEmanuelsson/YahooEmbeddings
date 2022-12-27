785076cad4438b246ef121216aa31512bb128f8d
Theodor Emanuelsson <49917684+TheodorEmanuelsson@users.noreply.github.com>
2022-12-27 11:37:18 +0100

    ws2

--- models.py
+++ models.py
@@ -31,16 +31,15 @@ class ParagraphEmbedding(BaseEstimator, TransformerMixin):
         return [token.text for token in nlp(sentence) if not token.is_stop]
 
 class DistributedBagOfWords(ParagraphEmbedding):
-    def __init__(self, lemmatize:bool=False, lowercase:bool=False, remove_stopwords:bool=False, use_mean:bool=False):
+    def __init__(self, lemmatize:bool=False, lowercase:bool=False, remove_stopwords:bool=False):
         self.lemmatize = lemmatize
         self.lowercase = lowercase
-        self.remove_stopwords = remove_stopwords,
-        self.use_mean = use_mean
+        self.remove_stopwords = remove_stopwords
 
     # Vectorize a block of text
     def _transform1(self, sentence):
 
-        # Sum the word vectors for each word in the sentence
+        # Get the tokenized list of words
         words = self._tokenize(sentence)
 
         # If the sentence is empty, return a vector of zeros
@@ -50,10 +49,7 @@ class DistributedBagOfWords(ParagraphEmbedding):
         # Get the word vectors for each word in the sentence
         word_vectors = [normalize_vec(nlp.vocab[word].vector) for word in words]
 
-        # If use_mean is True, return the mean of the word vectors
-        if self.use_mean:
-            return np.mean(word_vectors, axis=0)
-        # Otherwise, return the sum of the word vectors
+        # Return the sum of the word vectors
         return np.sum(word_vectors, axis=0)
 
     # Vectorize a single row of the dataframe.
@@ -68,3 +64,101 @@ class DistributedBagOfWords(ParagraphEmbedding):
     def transform(self, X):
         # Use numpy's apply_along_axis function to apply _transform2 to all rows of X
         return np.apply_along_axis(self._transform2, 1, X)  
+    
+class MeanPooling(ParagraphEmbedding):
+    def __init__(self, lemmatize:bool=False, lowercase:bool=False, remove_stopwords:bool=False):
+        self.lemmatize = lemmatize
+        self.lowercase = lowercase
+        self.remove_stopwords = remove_stopwords
+        
+    # Vectorize a block of text
+    def _transform1(self, sentence):
+
+        # Get the tokenized list of words
+        words = self._tokenize(sentence)
+
+        # If the sentence is empty, return a vector of zeros
+        if len(words) == 0:
+            return np.zeros(300)
+
+        # Get the normalized word vectors for each word in the sentence
+        word_vectors = [normalize_vec(nlp.vocab[word].vector) for word in words]
+
+        # Return the mean of the word vectors
+        return np.mean(word_vectors, axis=0)
+    
+    # Vectorize a single row of the dataframe.
+    def _transform2(self, row):
+
+        # Concatenate the sentence vectors
+        sent1_vec = self._transform1(row[0])
+        sent2_vec = self._transform1(row[1])
+        sent3_vec = self._transform1(row[2])
+        return np.concatenate((sent1_vec, sent2_vec, sent3_vec), axis=0)
+    
+    def transform(self, X):
+        # Use numpy's apply_along_axis function to apply _transform2 to all rows of X
+        return np.apply_along_axis(self._transform2, 1, X)
+
+class PCA_Projection(ParagraphEmbedding):
+    def __init__(self, lemmatize:bool=False, lowercase:bool=False, remove_stopwords:bool=False):
+        self.lemmatize = lemmatize
+        self.lowercase = lowercase
+        self.remove_stopwords = remove_stopwords
+
+    def _pca_projection(self, sentence_vector):
+        # Demean the sentence vector
+        norm_vec = sentence_vector - sentence_vector.mean()
+        # Get the covariance matrix
+        covariance_matrix = np.cov(norm_vec, rowvar=False)
+        # Get the eigenvalues and eigenvectors of the covariance matrix
+        eigen_vals, eigen_vecs = np.linalg.eigh(covariance_matrix)
+        # Get the sorted eigenvalues and eigenvectors
+        idx_sorted = np.argsort(eigen_vals)
+        idx_sorted_decreasing = list(reversed(idx_sorted))
+        eigen_vals_sorted = eigen_vals[idx_sorted_decreasing]
+        eigen_vecs_sorted = eigen_vecs[:, idx_sorted_decreasing]
+        # Get the first eigenvector
+        eigen_vecs_subset = eigen_vecs_sorted[:, :1]
+        # Project the sentence vector onto the first eigenvector
+        X_reduced = np.dot(eigen_vecs_subset.T, norm_vec.T).T
+
+        return X_reduced
+
+    # Vectorize a block of text
+    def _transform1(self, sentence):
+
+        # Get the tokenized list of words
+        words = self._tokenize(sentence)
+
+        # If the sentence is empty, return a vector of zeros
+        if len(words) == 0:
+            return np.zeros((300,1))
+
+        # Get the word vectors for each word in the sentence in (300,1) shape
+        word_vectors = [normalize_vec(nlp.vocab[word].vector).reshape(-1,1) for word in words]
+
+        word_vectors = np.concatenate(word_vectors, axis=1)
+
+        # If the sentence has more than one word, project the word vectors onto the first eigenvector
+        if word_vectors.shape[1] > 1:
+            # Projection of the word vectors onto the first eigenvector
+            projected_vector = self._pca_projection(word_vectors) 
+        # If the sentence has only one word, return the word vector
+        else:
+            projected_vector = word_vectors
+    
+        return projected_vector
+
+    # Vectorize a single row of the dataframe.
+    def _transform2(self, row):
+
+        # Concatenate the sentence vectors
+        sent1_vec = self._transform1(row[0])
+        sent2_vec = self._transform1(row[1])
+        sent3_vec = self._transform1(row[2])
+        return np.concatenate((sent1_vec, sent2_vec, sent3_vec), axis=0)
+    
+    def transform(self, X):
+        # Use numpy's apply_along_axis function to apply _transform2 to all rows of X
+        return np.apply_along_axis(self._transform2, 1, X).squeeze()
